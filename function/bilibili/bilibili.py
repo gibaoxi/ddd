8fb1ef4e2de1815cd7627f9c4cf766c03c2c65e0
gibaoxi <100911189+gibaoxi@users.noreply.github.com>
2025-03-24 14:11:48 +0800

    Update bilibili.py

--- function/bilibili/bilibili.py
+++ function/bilibili/bilibili.py
@@ -414,12 +414,11 @@ if __name__ == "__main__":
         BiliBiliCheckIn(bilibili_cookie_list=_bilibili_cookie_list).main()
     elif os.environ.get('BILI_COOKIE'):
         BILI_COOKIE = os.environ['BILI_COOKIE']
-        print (BILI_COOKIE)
         # 分割多个Cookie，每个账号的Cookie用&&分隔
         cookies_list = BILI_COOKIE.split('&&')
         for i, cookie_str in enumerate(cookies_list, 1):
             cookie_str = cookie_str.strip()
-            sendNotify.send(title=u"哔哩cookie", msg=cookie_str)
+            # sendNotify.send(title=u"哔哩cookie", msg=cookie_str)
             if not cookie_str:
                 continue
             # 解析单个账号的Cookie
@@ -429,8 +428,6 @@ if __name__ == "__main__":
                 if '=' in c:
                     key, val = c.split('=', 1)
                     cookie_dict[key.strip()] = val.strip()
-                    if i == 2:
-                        sys.exit(0)
             # 执行签到
             print(f"正在处理第 {i} 个账号")
             BiliBiliCheckIn(bilibili_cookie_list=cookie_dict).main(cookie_str)
